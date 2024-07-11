547ca9c9dc02c9d715d6a5c866a2dd05aca228a9
doginsouth <yew19@mcmaster.ca>
2024-07-11 12:27:47 -0400

    change product

--- src/views/LandingPages/Products/Sections/AuthorPosts.vue
+++ src/views/LandingPages/Products/Sections/AuthorPosts.vue
@@ -14,38 +14,38 @@ import post4 from "@/assets/img/examples/blog2.jpg";
     <div class="container">
       <div class="row">
         <div class="col-lg-6">
-          <h3 class="mb-5">Check my latest blogposts</h3>
+          <h3 class="mb-5">Our Products</h3>
         </div>
       </div>
       <div class="row">
-        <div class="col-lg-3 col-sm-6">
+        <div class="col-lg-4 col-sm-6">
           <TransparentBlogCard
             :image="post1"
             title="Rover raised $65 million"
             description="Finding temporary housing for your dog should be as easy as renting an Airbnb. That’s the idea behind Rover ..."
           />
         </div>
-        <div class="col-lg-3 col-sm-6">
+        <div class="col-lg-4 col-sm-6">
           <TransparentBlogCard
             :image="post2"
             title="MateLabs machine learning"
             description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
           />
         </div>
-        <div class="col-lg-3 col-sm-6">
+        <div class="col-lg-4 col-sm-6">
           <TransparentBlogCard
             :image="post3"
             title="MateLabs machine learning"
             description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
           />
         </div>
-        <div class="col-lg-3 col-md-12 col-12">
+        <!-- <div class="col-lg-3 col-md-12 col-12">
           <BackgroundBlogCard
             :image="post4"
             title="Flexible work hours"
             description="Rather than worrying about switching offices every couple years, you stay in the same place."
           />
-        </div>
+        </div> -->
       </div>
     </div>
   </section>
