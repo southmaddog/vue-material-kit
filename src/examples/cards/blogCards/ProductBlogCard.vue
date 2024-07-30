4f2164fd8fc37914e7ecc8d8abf3a2d98c845332
doginsouth <108967417+southmaddog@users.noreply.github.com>
2024-07-30 17:07:11 -0400

    product page edit
    add productcard in the blog card sectons, also transfer cards into product cards

---
+++ src/examples/cards/blogCards/ProductBlogCard.vue
@@ -0,0 +1,60 @@
+<script setup>
+defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  action: {
+    type: Object,
+    route: String,
+    color: String,
+    label: String,
+    default: () => ({
+      route: "javascript:;",
+      color: "success",
+      label: "Buy now",
+    }),
+  },
+});
+</script>
+
+<template>
+  <div class="card card-plain">
+    <div class="card-header p-0 position-relative">
+      <a class="d-block blur-shadow-image">
+        <img
+          :src="image"
+          :alt="name"
+          class="img-fluid shadow border-radius-lg"
+          loading="lazy"
+        />
+      </a>
+    </div>
+    <div class="card-body px-0">
+      <h5>
+        <a :href="action.route" class="text-dark font-weight-bold">{{
+          name
+        }}</a>
+      </h5>
+      <p class="text-lg text-gray-700 font-semibold">
+        {{ price }}
+      </p>
+      <a
+        :href="action.route"
+        class="text-sm icon-move-right"
+        :class="`text-${action.color}`"
+      >
+        {{ action.label }}
+        <i class="fas fa-arrow-right text-xs ms-1"></i>
+      </a>
+    </div>
+  </div>
+</template>
--- src/views/LandingPages/Products/Sections/AuthorPosts.vue
+++ src/views/LandingPages/Products/Sections/AuthorPosts.vue
@@ -1,14 +1,16 @@
+<!-- MainComponent.vue -->
 <script setup>
 // example components
-import TransparentBlogCard from "../../../../examples/cards/blogCards/TransparentBlogCard.vue";
-import BackgroundBlogCard from "../../../../examples/cards/blogCards/BackgroundBlogCard.vue";
+import ProductBlogCard from "../../../../examples/cards/blogCards/ProductBlogCard.vue";
+// import BackgroundBlogCard from "../../../../examples/cards/blogCards/BackgroundBlogCard.vue";
 
-//Vue Material Kit 2 components
+// Vue Material Kit 2 components
 import post1 from "@/assets/img/examples/testimonial-6-2.jpg";
 import post2 from "@/assets/img/examples/testimonial-6-3.jpg";
 import post3 from "@/assets/img/examples/blog-9-4.jpg";
 import post4 from "@/assets/img/examples/blog2.jpg";
 </script>
+
 <template>
   <section class="py-3">
     <div class="container">
@@ -19,24 +21,27 @@ import post4 from "@/assets/img/examples/blog2.jpg";
       </div>
       <div class="row">
         <div class="col-lg-4 col-sm-6">
-          <TransparentBlogCard
+          <ProductBlogCard
             :image="post1"
-            title="Rover raised $65 million"
-            description="Finding temporary housing for your dog should be as easy as renting an Airbnb. That’s the idea behind Rover ..."
+            name="Product 1"
+            price="$65.00"
+            :action="{ route: '#', color: 'success', label: 'Buy now' }"
           />
         </div>
         <div class="col-lg-4 col-sm-6">
-          <TransparentBlogCard
+          <ProductBlogCard
             :image="post2"
-            title="MateLabs machine learning"
-            description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
+            name="Product 2"
+            price="$75.00"
+            :action="{ route: '#', color: 'success', label: 'Buy now' }"
           />
         </div>
         <div class="col-lg-4 col-sm-6">
-          <TransparentBlogCard
+          <ProductBlogCard
             :image="post3"
-            title="MateLabs machine learning"
-            description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
+            name="Product 3"
+            price="$85.00"
+            :action="{ route: '#', color: 'success', label: 'Buy now' }"
           />
         </div>
         <!-- <div class="col-lg-3 col-md-12 col-12">
